6f67e2280abb89ce4c95f274db0502087c1646bb
Matt McFarland <contact@mattmcfarland.com>
2017-03-23 11:07:51 -0400

    Cleanup build script, make more readable.

--- scripts/build.js
+++ scripts/build.js
@@ -1,35 +1,27 @@
 #!/usr/bin/env node
-require('events').EventEmitter.prototype._maxListeners = 100;
-const inspect = require('util').inspect
-const { resolve: resolvePath } = require('path')
-const { createWriteStream, writeFile } = require('fs')
-const { Readable } = require('stream')
-const { parse: parseYAML } = require('yamljs')
-const chalk = require('chalk')
 
+require('events').EventEmitter.prototype._maxListeners = 100;
+
+const fs = require('fs')
+const YAML = require('yamljs')
+const chalk = require('chalk')
 const plist = require('plist')
 const yamlLoader = require('./yaml-import-loader')
 
 yamlLoader('src/syntaxes/_index.yaml', (err, res) => {
-  if (err) {
-    throw new Error(err)
-  } else {
-    const yamlString = res.buffer;
-    const pojo = parseYAML(yamlString)
-    const jsonString = JSON.stringify(pojo, null, 4)
-    const plistString = plist.build(pojo)
+  if (err) throw new Error(err)
 
-    const builds = {
-      'lib/syntaxes/ManiaScript.YAML-tmLanguage': yamlString,
-      'lib/syntaxes/ManiaScript.JSON-tmLanguage': jsonString,
-      'lib/syntaxes/ManiaScript.tmLanguage': plistString
-    }
+  const yamlString = res.buffer;
+  const pojo = YAML.parse(yamlString)
+  const jsonString = JSON.stringify(pojo, null, 4)
+  const plistString = plist.build(pojo)
+  const builds = [
+    ['lib/syntaxes/ManiaScript.YAML-tmLanguage', yamlString],
+    ['lib/syntaxes/ManiaScript.JSON-tmLanguage', jsonString],
+    ['lib/syntaxes/ManiaScript.tmLanguage', plistString]
+  ]
 
-    Object.keys(builds).map((path) => {
-      const buffer = builds[path];
-      writeFile(path, buffer, 'utf8', () => {
-        console.log(chalk.green(`${path}... done!`))
-      });      
-    })
-  }
+  builds.forEach(([path, buffer]) => 
+    fs.writeFile(path, buffer, 'utf8', 
+      console.log(chalk.yellow('write'), chalk.green(`${path}... done!`))))
 })
